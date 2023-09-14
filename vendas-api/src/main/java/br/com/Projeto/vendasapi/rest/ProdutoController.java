d739726cff84f4bfbc4904a547484eccc66abd67
marcus aurelio <marcusaurelio743@gmail.com>
2023-09-13 22:11:04 -0300

    salvando produtos usando a  api

--- vendas-api/src/main/java/br/com/Projeto/vendasapi/rest/ProdutoController.java
+++ vendas-api/src/main/java/br/com/Projeto/vendasapi/rest/ProdutoController.java
@@ -1,6 +1,7 @@
 package br.com.Projeto.vendasapi.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import br.com.Projeto.vendasapi.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin("*")
 public class ProdutoController {
 	
 	@Autowired
